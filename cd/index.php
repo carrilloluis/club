88e95b468003823b324c02f9ab2040ff14b10c07
Luis Carrillo Gutiérrez <carrilloluis@users.noreply.github.com>
2026-07-11 18:56:19 -0400

    Update index.php

--- cd/index.php
+++ cd/index.php
@@ -6,88 +6,87 @@
 
 define('QUERYSTRING_KEYWORD', 'taskID');
 define('STANDARD_HEADER', 'Content-Type: application/json; charset="UTF-8"');
-define('DSN', 'sqlite:/tmp/data');
+define('DSN', 'sqlite:/tmp/data.db');
 define('SOURCE_DATA', 'php://input');
 
 global $HTTP_STATUS;
 global $rs;
 
-    $startTime = microtime(true);
-    $startMemory = memory_get_usage();
-    $request_ = strtoupper(filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_ENCODED));
+require_once 'ClassModel.php';
 
-    try {
-        $connection2db_ = new PDO(DSN);
-        $connection2db_->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $error) {
-        http_response_code(500);
-        die(json_encode( array('error' => $error->getMessage() )));
-    } catch(Exception $errorConnection) {
-        http_response_code(500);
-        die(json_encode( array('error' => $errorConnection->getMessage() )));
-    }
+	$startTime = microtime(true);
+	$startMemory = memory_get_usage();
+	$request_ = strtoupper(filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_ENCODED));
 
-    if ($request_ === 'GET')
-    {
-        $action2perform_ = @intval($_GET[QUERYSTRING_KEYWORD]) < 1 ? 1 : @intval($_GET[QUERYSTRING_KEYWORD]);
-        switch ( $action2perform_ ) {
-            case 1 : {
-                $page_ = @intval($_GET['page_']) < 1 ? 1 : @intval($_GET['page_']);
-                $numberOf_ = @intval($_GET['nitems_']) < 1 ? 100 : @intval($_GET['nitems_']);
-                
-                # LIST_ITEMS per ID
-                require_once '.php';
-                $rs = getContactData($connection2db_, $id_, $page_, $numberOf_);
-                $HTTP_STATUS = 200;
-                break;
-            }
-            case 2 : { 
-                $id_ = $_GET['requestedID'];
-                if (!is_numeric($id_)) {
-                    http_response_code(403);
-                    header(STANDARD_HEADER);
-                    die( json_encode([ 'error' => 'parameters error on ID requested [ERROR]' ]));
-                }
+	try {
+		$connection2db_ = new PDO(DSN);
+		$connection2db_->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $error) {
+		http_response_code(500);
+		die(json_encode( array('error' => $error->getMessage() )));
+	} catch(Exception $errorConnection) {
+		http_response_code(500);
+		die(json_encode( array('error' => $errorConnection->getMessage() )));
+	}
 
-                # SPECIFIC data item from ASSOCIATE
-                require_once '.php';
-                $rs = getContactDataById($connection2db_, @intval($id_));
-                $HTTP_STATUS = 200;
-                break;
-            }
-            case 3 : { 
-                $querystring2search_ = @strval(explode(' ', $_GET['querySTR'])[0]);
-                if (strlen($querystring2search_) < 5) {
-                    http_response_code(403);
-                    header(STANDARD_HEADER);
-                    die( json_encode([ 'error' => 'ERROR on parameters to SEARCH' ]));
-                }
+	if ($request_ === 'GET')
+	{
+		$action2perform_ = @intval($_GET[QUERYSTRING_KEYWORD]) < 1 ? 1 : @intval($_GET[QUERYSTRING_KEYWORD]);
+		switch ( $action2perform_ ) {
+			case 1 : {
+				$page_ = @intval($_GET['page_']) < 1 ? 1 : @intval($_GET['page_']);
+				$numberOf_ = @intval($_GET['nitems_']) < 1 ? 100 : @intval($_GET['nitems_']);
+				$id_ = @intval($_GET['id_']);
+				if (!is_numeric($id_) || $id_ < 1000) {
+					http_response_code(403);
+					header(STANDARD_HEADER);
+					die( json_encode([ 'error' => 'parameters error on ID requested [ERROR]' ]));
+				}
+				$rs = (new ClassModel($connection2db_))->getContactData($id_, $page_, $numberOf_);
+				$HTTP_STATUS = 200;
+				break;
+			}
+			case 2 : { 
+				$id_ = @intval($_GET['id_']);
+				if (!is_numeric($id_) || $id_ < 1000) {
+					http_response_code(403);
+					header(STANDARD_HEADER);
+					die( json_encode([ 'error' => 'parameters error on ID requested [ERROR]' ]));
+				}
+				$rs = (new ClassModel($connection2db_))->getContactDataById($id_);
+				$HTTP_STATUS = 200;
+				break;
+			}
+			case 3 : { 
+				$querystring2search_ = @strval(explode(' ', $_GET['querySTR'])[0]);
+				if (strlen($querystring2search_) < 3) {
+					http_response_code(403);
+					header(STANDARD_HEADER);
+					die( json_encode([ 'error' => 'ERROR on parameters to SEARCH' ]));
+				}
+				$rs = (new ClassModel($connection2db_))->getContactDataByValue($querystring2search_);
+				$HTTP_STATUS = 200;
+				break;
+			}
+			default : {
+				break;
+			}
+		}
+	}
 
-                # // Search
-                require_once '.php';
-                $rs = getContactDataByValue($connection2db_, $querystring2search_);
-                $HTTP_STATUS = 200;
-                break;
-            }
-            default : {
-                break;
-            }
-        }
-    }
+	$connection2db_ = null;
+	unset($connection2db_);
 
-    $connection2db_ = null;
-    unset($connection2db_);
-
-    http_response_code(isset($HTTP_STATUS) ? $HTTP_STATUS : 500);
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Origin');
-    
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Credentials: true');
-    header(STANDARD_HEADER);
-    echo json_encode([
-        'delay' => number_format(microtime(true) - $startTime, 4) . ' ms',
-        'memory' => round((memory_get_usage() - $startMemory) / (1024 * 1024), 3) . ' MB',
-        'data' => isset($rs) ? $rs : [],
-    ]);
+	http_response_code(isset($HTTP_STATUS) ? $HTTP_STATUS : 500);
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Origin');
+	
+	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+	header('Access-Control-Allow-Credentials: true');
+	header(STANDARD_HEADER);
+	echo json_encode([
+		'delay' => number_format(microtime(true) - $startTime, 4) . ' ms',
+		'memory' => round((memory_get_usage() - $startMemory) / (1024 * 1024), 3) . ' MB',
+		'data' => isset($rs) ? $rs : [],
+	]);
 
